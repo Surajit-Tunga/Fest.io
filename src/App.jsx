ffcc7480cd48f45ac3ca19521df3a283692bbc23
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-23 16:18:37 +0530

    created about

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
-import CountDownTimer from './components/CountDownTimer'
+import bghero from "./assets/bghero.mp4"
+import About from './components/About'
 const App = () => {
   return (
     <>
-      <NavBar/>
-      <Hero/>
+    <div>
+      <div className='h-[700px]  relative '>
+        <video autoPlay loop muted src={bghero}className=' fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]'></video>
+        <NavBar/>
+        <Hero/>
+        <About/>
+      </div>
       
+    </div>
     </>
+
     
   )
 }
 
 export default App
+
+
+
+
