0aaa9d412e75e86b617eaead89cb2e935c8ee32b
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-24 18:40:18 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,5 @@
 import Chair from "./team/Chair"
 import Ccv from "./team/Ccv"
-import Website from "./team/Website"
-import Market from "./team/Market"
 import Voll from "./team/Voll"
 import React, { useEffect } from "react";
 import AOS from "aos";
@@ -28,8 +26,6 @@ const CoreTeam = () => {
       <div className="max-w-3xl mx-auto text-center">
         <Chair/>
         <Ccv/>
-        <Website/>
-        <Market/>
         <Voll/>
       </div>
     </section>
